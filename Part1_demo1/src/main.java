78b6bbbcbc1707b2fbad8380da55abc35005bb92
nvpthao311 <vothao1132002@gmail.com>
2025-03-10 21:00:50 +0700

    Add new demo files and remove test.txt

---
+++ Part1_demo1/src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+
+        IntegerArrayHandler arrayA = new IntegerArrayHandler();
+        arrayA.EnterNumber();
+        //arrayA.CalculateSum();
+        //arrayA.EvenOddChecker();
+        arrayA.FindMaxMin(false);
+
+    }
+}
