447c07b6d6d131eb63f7e0787a35ca3a0ac311ed
nvpthao311 <vothao1132002@gmail.com>
2025-04-12 14:16:57 +0700

    remove

--- Part3_demo1/src/main/java/org/example/Maytinhbotui.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public interface Maytinhbotui {
-
-    public double cong (double a, double b);
-    public double tru (double a, double b);
-    public  double nhan (double a, double b);
-    public  double chia (double a, double b);
-}
--- Part3_demo1/src/main/java/org/example/Sapxep.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.lang.reflect.Array;
-
-public interface Sapxep {
-    public void tangdan (double[] arr);
-    public void giamdan (double[] arr);
-
-}
