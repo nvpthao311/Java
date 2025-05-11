342b4a684e2aafbb3fccfa1bbb52bd4744fab0b5
nvpthao311 <vothao1132002@gmail.com>
2025-05-12 01:05:37 +0700

    12/05

--- MusicManager_4/src/main/java/com/example/demo/Exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Exception;
-
-public class NotFoundException extends RuntimeException{
-
-    public NotFoundException(Long id, String entity){
-        super ( entity + " with ID: " + id + " not found!");
-    }
-}
--- MusicManager_4/src/main/java/com/example/demo/Model/Role.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.Model;
-
-public enum Role {
-    ROLE_USER,ROLE_ADMIN
-}
